00e2e2f67963ec982c7ef961d3a9ff38b2070961
Deepanshu8019 <deepanshushuka2723@gmail.com>
2024-07-05 20:19:26 +0530

    Final Commit

--- Frontend/src/services/apis.js
+++ Frontend/src/services/apis.js
@@ -1,5 +1,4 @@
-const BASE_URL = "http://localhost:4000/api/v1"
-// const BASE_URL = "https://studyocean.onrender.com/api/v1"
+const BASE_URL = "https://studyocean.onrender.com/api/v1"
 
 // AUTH ENDPOINTS
 export const endpoints = {
